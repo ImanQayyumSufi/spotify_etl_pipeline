dc7bdb4495c56cbf59ef0f4a1adab68d1b9527f3
ImanQayyumSufi <150775132+ImanQayyumSufi@users.noreply.github.com>
2024-08-09 22:25:37 +0800

    add working files

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import boto3
+from datetime import datetime
+
+def lambda_handler(event, context):
+    client_id = os.environ.get('client_id')
+    client_secret = os.environ.get('client_secret')
+    
+    # Setting up the credentials for authentication
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+
+    # To interact with the spotify API
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    
+    # This is the link to get top 50 global spotify songs
+    spotify_playlist = 'https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF'
+
+    # We want to get the endpoint of the link
+    playlist_URI = spotify_playlist.split("/")[4]
+    
+    data = sp.playlist_tracks(playlist_URI)
+    
+    client = boto3.client('s3')
+    
+    filename = "spotify_raw_" + str(datetime.now()) + ".json"
+    
+    client.put_object(
+        Bucket="spotify-etl-project-iman",
+        Key="bronze_layer/to_processed/" + filename,
+        Body=json.dumps(data)
+    )
